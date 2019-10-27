b48e3f1c974982c54ad0fe11a167671dfefc2299
Henner Bendig <h.bendig@stud.hs-wismar.de>
2019-10-27 13:57:13 +0100

    first commit

---
+++ MYKCalculator/MainWindow.xaml.cs
@@ -0,0 +1,269 @@
+﻿using ExcelDataReader;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Threading;
+using System.Windows;
+
+namespace MYKCalculator
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string dataPath = "";
+
+        private DataSet loadedDataSet;
+        private int selectedTableIndex;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cmbbox_TableChooser.SelectionChanged += Cmbbox_TableChooser_SelectionChanged;
+        }
+
+        private void Cmbbox_TableChooser_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            selectedTableIndex = cmbbox_TableChooser.SelectedIndex;
+            LoadSelectedDataTable(loadedDataSet.Tables[selectedTableIndex].Copy());
+        }
+
+        private void btn_xlsPickerButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Worksheets|*.xls;*.xlsx;*.xlsm";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                dataPath = lbl_Filepath.Text = openFileDialog.FileName;
+                btn_ImportData.IsEnabled = true;
+            }
+        }
+
+
+
+        private void btn_ImportData_Click(object sender, RoutedEventArgs e)
+        {
+            Thread importThread = new Thread(new ThreadStart(ImportXLSData));
+            importThread.Start();
+        }
+
+        private void ImportXLSData()
+        {
+            using (var stream = File.Open(dataPath, FileMode.Open, FileAccess.Read))
+            {
+                // Auto-detect format, supports:
+                //  - Binary Excel files (2.0-2003 format; *.xls)
+                //  - OpenXml Excel files (2007 format; *.xlsx)
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    // Use the AsDataSet extension method
+                    loadedDataSet = reader.AsDataSet();
+
+                    // Combobox zur Tabellenwahl füllen
+                    List<string> tableNames = new List<string>();
+                    foreach (DataTable dataTable in loadedDataSet.Tables)
+                    {
+                        tableNames.Add(dataTable.TableName);
+                    }
+
+                    Dispatcher.Invoke((() => FillCombobox(tableNames))); ;
+                }
+            }
+        }
+
+        private void FillCombobox(List<string> tableNames)
+        {
+            cmbbox_TableChooser.ItemsSource = tableNames;
+            cmbbox_TableChooser.IsEnabled = true;
+            cmbbox_TableChooser.SelectedIndex = 0;
+        }
+
+        private void LoadSelectedDataTable(DataTable loadedTable)
+        {
+            dgrid_DataGrid.DataContext = loadedTable;
+            SetColumnNames(loadedTable);
+
+            loadedTable.Rows[0].Delete();
+            loadedTable.AcceptChanges();
+            btn_CalculateScore.IsEnabled = true;
+        }
+
+        private void SetColumnNames(DataTable data)
+        {
+            for (int n = 0; n < dgrid_DataGrid.Columns.Count; n++)
+            {
+                dgrid_DataGrid.Columns[n].Header = data.Rows[0].ItemArray[n];
+            }
+        }
+
+        private void CalculateScore()
+        {
+            int currentScore = 0;
+            int currentRowIndex = 1;
+
+            for (; currentRowIndex < loadedDataSet.Tables[selectedTableIndex].Rows.Count; currentRowIndex++)
+            {
+                currentScore = 0;
+                DataRow currentRow = loadedDataSet.Tables[selectedTableIndex].Rows[currentRowIndex];
+
+                // single
+                int singleColumnIndex = FindIndexToColumn("single");
+                if (currentRow[singleColumnIndex].ToString() == "1")
+                {
+                    currentScore++;
+                }
+
+                // from Pos
+                int fromPosIndex = FindIndexToColumn("from_pos");
+                float outFromPos;
+                if (float.TryParse(currentRow[fromPosIndex].ToString(), out outFromPos))
+                {
+                    if (outFromPos < 127640739.7 || outFromPos > 130113749.6)
+                        currentScore++;
+                }
+
+                // Vaf_Mean
+                int vafMeanIndex = FindIndexToColumn("Vaf_Mean");
+                float outVafMean;
+                if (float.TryParse(currentRow[vafMeanIndex].ToString(), out outVafMean))
+                {
+                    if (outVafMean <= 0.03)
+                        currentScore++;
+                }
+                else
+                {
+                    int vafDiffIndex = FindIndexToColumn("Vaf_Differenz");
+                    float outVafDiff;
+                    if (float.TryParse(currentRow[vafDiffIndex].ToString(), out outVafDiff))
+                    {
+                        if (outVafDiff >= 0.136350466)
+                        {
+                            int vafSR_Index = FindIndexToColumn("vaf_SR");
+                            int vafPR_Index = FindIndexToColumn("vaf_PR");
+                            float outVafSR;
+                            float outVafPR;
+                            if (float.TryParse(currentRow[vafSR_Index].ToString(), out outVafSR))
+                            {
+                                if (outVafSR <= 0.03)
+                                    currentScore++;
+                            }
+                            else if (float.TryParse(currentRow[vafPR_Index].ToString(), out outVafPR))
+                            {
+                                if (outVafPR <= 0.03)
+                                    currentScore++;
+                            }
+                        }
+                    }
+                }
+
+                // Reads_Mean
+                int readsMeanIndex = FindIndexToColumn("Reads_Mean");
+                float outReadsMean;
+                if (float.TryParse(currentRow[readsMeanIndex].ToString(), out outReadsMean))
+                {
+                    if (outReadsMean <= 5)
+                        currentScore++;
+                }
+                else
+                {
+                    int readsDiffIndex = FindIndexToColumn("Reads_Differenz");
+                    float outReadsDif;
+                    if (float.TryParse(currentRow[readsDiffIndex].ToString(), out outReadsDif))
+                    {
+                        if (outReadsDif >= 24.55288628)
+                        {
+                            int readsSR_Index = FindIndexToColumn("tumor_alt_SR");
+                            int readsPR_Index = FindIndexToColumn("tumor_alt_PR");
+                            float outReadsSR;
+                            float outReadsPR;
+                            if (float.TryParse(currentRow[readsSR_Index].ToString(), out outReadsSR))
+                            {
+                                if (outReadsSR <= 5)
+                                    currentScore++;
+                            }
+                            else if (float.TryParse(currentRow[readsPR_Index].ToString(), out outReadsPR))
+                            {
+                                if (outReadsPR <= 5)
+                                    currentScore++;
+                            }
+                        }
+                    }
+                }
+
+
+                // MYC Expression
+                int mycExpressionIndex = FindIndexToColumn("MYC Expression");
+                float outMycExp;
+                if (float.TryParse(currentRow[mycExpressionIndex].ToString(), out outMycExp))
+                {
+                    if (outMycExp <= 4.46)
+                        currentScore++;
+                }
+
+                // n_myl
+                int nMylIndex = FindIndexToColumn("n_myl");
+                float outNMyl;
+                if (float.TryParse(currentRow[nMylIndex].ToString(), out outNMyl))
+                {
+                    if (outNMyl == 2)
+                    {
+                        currentScore++;
+                    }
+                    else if (outNMyl > 2)
+                    {
+                        currentScore++;
+                        currentScore++;
+                    }
+                }
+
+
+
+                SetScore(currentScore, currentRowIndex);
+            }
+
+            // Refresh data
+            LoadSelectedDataTable(loadedDataSet.Tables[selectedTableIndex].Copy());
+        }
+
+        private void SetScore(int scoreToSet, int rowIndex)
+        {
+            int scoreColumnIndex = FindIndexToColumn("Score");
+            loadedDataSet.Tables[selectedTableIndex].Rows[rowIndex].ItemArray[scoreColumnIndex] = scoreToSet;
+            loadedDataSet.Tables[selectedTableIndex].Rows[rowIndex].SetField(scoreColumnIndex, scoreToSet);
+            loadedDataSet.Tables[selectedTableIndex].Rows[rowIndex].AcceptChanges();
+        }
+
+        private int FindIndexToColumn(string searchKeyword)
+        {
+            List<string> headerList = new List<string>();
+            foreach (object obj in loadedDataSet.Tables[selectedTableIndex].Rows[0].ItemArray)
+            {
+                headerList.Add(obj.ToString());
+            }
+
+            return headerList.FindIndex(x => x.Equals(searchKeyword));
+        }
+
+        private void btn_CalculateScore_Click(object sender, RoutedEventArgs e)
+        {
+            CalculateScore();
+           
+        }
+
+        private void ExportDataSetToExcel(DataSet ds)
+        {
+            dgrid_DataGrid.SelectAll();
+            //DataObject dataObj = dgrid_DataGrid.GetClipboardContent();
+           // if (dataObj != null)
+             //   Clipboard.SetDataObject(dataObj);
+        }
+
+        private void btn_ExportData_Click(object sender, RoutedEventArgs e)
+        {
+            ExportDataSetToExcel(loadedDataSet);
+        }
+    }
+}
---
+++ MYKCalculator/Spreadsheet.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYKCalculator
+{
+    class Spreadsheet
+    {
+        private string sv_cluster;
+        private string array_id;
+        private string lab_id;
+        private string entitaet;
+        private string score;
+        private string numberOf;
+        private string single;
+        private string reziprok;
+        private string komplex;
+        private string from_pos;
+        private string from_direct;
+        private string to_chrom;
+        private string chr_band;
+        private string to_pos;
+        private string to_direct;
+        private string partnergen;
+        private string tumor_ref_pr;
+        private string tumor_alt_pr;
+        private string vaf_PR;
+        private string tumor_ref_sr;
+        private string tumor_alt_sr;
+        private string vaf_sr;
+        private string vaf_mean;
+        private string vaf_differez;
+        private string reads_mean;
+        private string reads_differenz;
+        private string myc;
+        private string n_lym;
+        private string n_myl;
+        private string mll_freq;
+        private string fish;
+        private string signalkonstellation_myc;
+        private string fish_8_14;
+        private string signalkonstellation_8_14;
+        private string klongroeße;
+
+        public string Sv_cluster { get => sv_cluster; set => sv_cluster = value; }
+        public string Array_id { get => array_id; set => array_id = value; }
+        public string Lab_id { get => lab_id; set => lab_id = value; }
+        public string Entitaet { get => entitaet; set => entitaet = value; }
+        public string Score { get => score; set => score = value; }
+        public string NumberOf { get => numberOf; set => numberOf = value; }
+        public string Single { get => single; set => single = value; }
+        public string Reziprok { get => reziprok; set => reziprok = value; }
+        public string Komplex { get => komplex; set => komplex = value; }
+        public string From_pos { get => from_pos; set => from_pos = value; }
+        public string From_direct { get => from_direct; set => from_direct = value; }
+        public string To_chrom { get => to_chrom; set => to_chrom = value; }
+        public string Chr_band { get => chr_band; set => chr_band = value; }
+        public string To_pos { get => to_pos; set => to_pos = value; }
+        public string To_direct { get => to_direct; set => to_direct = value; }
+        public string Partnergen { get => partnergen; set => partnergen = value; }
+        public string Tumor_ref_pr { get => tumor_ref_pr; set => tumor_ref_pr = value; }
+        public string Tumor_alt_pr { get => tumor_alt_pr; set => tumor_alt_pr = value; }
+        public string Vaf_PR { get => vaf_PR; set => vaf_PR = value; }
+        public string Tumor_ref_sr { get => tumor_ref_sr; set => tumor_ref_sr = value; }
+        public string Tumor_alt_sr { get => tumor_alt_sr; set => tumor_alt_sr = value; }
+        public string Vaf_sr { get => vaf_sr; set => vaf_sr = value; }
+        public string Vaf_mean { get => vaf_mean; set => vaf_mean = value; }
+        public string Vaf_differez { get => vaf_differez; set => vaf_differez = value; }
+        public string Reads_mean { get => reads_mean; set => reads_mean = value; }
+        public string Reads_differenz { get => reads_differenz; set => reads_differenz = value; }
+        public string Myc { get => myc; set => myc = value; }
+        public string N_lym { get => n_lym; set => n_lym = value; }
+        public string N_myl { get => n_myl; set => n_myl = value; }
+        public string Mll_freq { get => mll_freq; set => mll_freq = value; }
+        public string Fish { get => fish; set => fish = value; }
+        public string Signalkonstellation_myc { get => signalkonstellation_myc; set => signalkonstellation_myc = value; }
+        public string Fish_8_14 { get => fish_8_14; set => fish_8_14 = value; }
+        public string Signalkonstellation_8_14 { get => signalkonstellation_8_14; set => signalkonstellation_8_14 = value; }
+        public string Klongroeße { get => klongroeße; set => klongroeße = value; }
+    }
+}
