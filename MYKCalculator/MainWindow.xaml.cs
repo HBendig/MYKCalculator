4ec28817a6ef99275565350d5ebe3dda053dd4cb
Henner Bendig <h.bendig@stud.hs-wismar.de>
2019-12-15 17:15:42 +0100

    mutationsanalyse

--- MYKCalculator/MainWindow.xaml.cs
+++ MYKCalculator/MainWindow.xaml.cs
@@ -1,34 +1,59 @@
 ﻿using ExcelDataReader;
 using Microsoft.Win32;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace MYCCalculator
 {
+    class Patient
+    { 
+        public string ArrayID;
+        public string Entitaet;
+        public Dictionary<string, int> GenListe = new Dictionary<string, int>();
+       
+    }
+
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         private string dataPath = "";
+        private string dataPath_mutations = "";
 
         private DataSet loadedDataSet;
+        private DataSet loadedDataSet_mutations;
         private int selectedTableIndex;
+        private int selectedTableIndex_mutations;
+
+        List<string> csvLines = new List<string>();
 
         public MainWindow()
         {
             InitializeComponent();
 
             cmbbox_TableChooser.SelectionChanged += Cmbbox_TableChooser_SelectionChanged;
+            cmbbox_TableChooser2.SelectionChanged += Cmbbox_TableChooser_mutations_SelectionChanged;
+
+            LoadingGrid.IsEnabled = false;
+            LoadingGrid.Visibility = Visibility.Hidden;
         }
 
         private void Cmbbox_TableChooser_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             selectedTableIndex = cmbbox_TableChooser.SelectedIndex;
             LoadSelectedDataTable(loadedDataSet.Tables[selectedTableIndex].Copy());
+        }
+        private void Cmbbox_TableChooser_mutations_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            selectedTableIndex_mutations = cmbbox_TableChooser2.SelectedIndex;
+            LoadSelectedDataTable_mutations(loadedDataSet_mutations.Tables[selectedTableIndex_mutations].Copy());
         }
 
         private void btn_xlsPickerButton_Click(object sender, RoutedEventArgs e)
@@ -42,7 +67,49 @@ namespace MYCCalculator
             }
         }
 
+        private void btn_xlsPickerButton2_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Worksheets|*.xls;*.xlsx;*.xlsm";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                dataPath_mutations = lbl_Filepath2.Text = openFileDialog.FileName;
+                btn_ImportData2.IsEnabled = true;
+            }
+        }
 
+        private void btn_ImportData2_Click(object sender, RoutedEventArgs e)
+        {
+            Thread importThread = new Thread(new ThreadStart(ImportXLSData_Mutations));
+            importThread.Start();
+        }
+
+        private void ImportXLSData_Mutations()
+        {
+            Dispatcher.Invoke(() => ShowLoadingScreen(true));
+
+            using (var stream = File.Open(dataPath_mutations, FileMode.Open, FileAccess.Read))
+            {
+                // Auto-detect format, supports:
+                //  - Binary Excel files (2.0-2003 format; *.xls)
+                //  - OpenXml Excel files (2007 format; *.xlsx)
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    // Use the AsDataSet extension method
+                    loadedDataSet_mutations = reader.AsDataSet();
+                }
+            }
+
+            // Combobox zur Tabellenwahl füllen
+            List<string> tableNames = new List<string>();
+            foreach (DataTable dataTable in loadedDataSet_mutations.Tables)
+            {
+                tableNames.Add(dataTable.TableName);
+            }
+
+            Dispatcher.Invoke((() => FillCombobox2(tableNames))); ;
+            Dispatcher.Invoke(() => ShowLoadingScreen(false));
+        }
 
         private void btn_ImportData_Click(object sender, RoutedEventArgs e)
         {
@@ -52,6 +119,8 @@ namespace MYCCalculator
 
         private void ImportXLSData()
         {
+            Dispatcher.Invoke(() => ShowLoadingScreen(true));
+
             using (var stream = File.Open(dataPath, FileMode.Open, FileAccess.Read))
             {
                 // Auto-detect format, supports:
@@ -72,6 +141,20 @@ namespace MYCCalculator
                     Dispatcher.Invoke((() => FillCombobox(tableNames))); ;
                 }
             }
+            Dispatcher.Invoke(() => ShowLoadingScreen(false));
+        }
+
+        private void ShowLoadingScreen(bool _isVisible)
+        {
+            if(_isVisible)
+            {
+                LoadingGrid.IsEnabled = true;
+                LoadingGrid.Visibility = Visibility.Visible;
+            } else
+            {
+                LoadingGrid.IsEnabled = false;
+                LoadingGrid.Visibility = Visibility.Hidden;
+            }
         }
 
         private void FillCombobox(List<string> tableNames)
@@ -79,6 +162,13 @@ namespace MYCCalculator
             cmbbox_TableChooser.ItemsSource = tableNames;
             cmbbox_TableChooser.IsEnabled = true;
             cmbbox_TableChooser.SelectedIndex = 0;
+        }
+        
+        private void FillCombobox2(List<string> tableNames)
+        {
+            cmbbox_TableChooser2.ItemsSource = tableNames;
+            cmbbox_TableChooser2.IsEnabled = true;
+            cmbbox_TableChooser2.SelectedIndex = 0;
         }
 
         private void LoadSelectedDataTable(DataTable loadedTable)
@@ -90,12 +180,28 @@ namespace MYCCalculator
             loadedTable.AcceptChanges();
             btn_CalculateScore.IsEnabled = true;
         }
+        private void LoadSelectedDataTable_mutations(DataTable loadedTable)
+        {
+            dgrid_DataGrid2.DataContext = loadedTable;
+            SetColumnNames_mutations(loadedTable);
+
+            loadedTable.Rows[0].Delete();
+            loadedTable.AcceptChanges();
+            btn_Analyse.IsEnabled = true;
+        }
 
         private void SetColumnNames(DataTable data)
         {
             for (int n = 0; n < dgrid_DataGrid.Columns.Count; n++)
             {
                 dgrid_DataGrid.Columns[n].Header = data.Rows[0].ItemArray[n];
+            }
+        }
+        private void SetColumnNames_mutations(DataTable data)
+        {
+            for (int n = 0; n < dgrid_DataGrid2.Columns.Count; n++)
+            {
+                dgrid_DataGrid2.Columns[n].Header = data.Rows[0].ItemArray[n];
             }
         }
 
@@ -281,17 +387,91 @@ namespace MYCCalculator
            
         }
 
-        private void ExportDataSetToExcel(DataSet ds)
-        {
-            dgrid_DataGrid.SelectAll();
-            //DataObject dataObj = dgrid_DataGrid.GetClipboardContent();
-           // if (dataObj != null)
-             //   Clipboard.SetDataObject(dataObj);
-        }
+        Dictionary<string, int> genList = new Dictionary<string, int>();
+        List<Patient> patientList = new List<Patient>();
 
-        private void btn_ExportData_Click(object sender, RoutedEventArgs e)
+        private void btn_Analyse_Click(object sender, RoutedEventArgs e)
         {
-            ExportDataSetToExcel(loadedDataSet);
+            List<string> alreadyChecked = new List<string>();
+
+            for (int currentRowIndex = 1; currentRowIndex < loadedDataSet_mutations.Tables[selectedTableIndex_mutations].Rows.Count; currentRowIndex++)
+            {
+                DataRow currentRow = loadedDataSet_mutations.Tables[selectedTableIndex_mutations].Rows[currentRowIndex];
+                Patient patient = new Patient();
+
+                patient.ArrayID = currentRow[0].ToString();
+                patient.Entitaet = currentRow[2].ToString();
+                
+
+                for (int currentColumnIndex = 4; currentColumnIndex < loadedDataSet_mutations.Tables[selectedTableIndex_mutations].Columns.Count; currentColumnIndex++)
+                {
+                    string currentGen = currentRow[currentColumnIndex].ToString();
+                    if (currentGen != "")
+                    {
+                        if (genList.ContainsKey(currentGen))
+                        {
+                            genList[currentGen]++;
+                        }
+                        else
+                        {
+                            genList.Add(currentGen, 1);
+                        }
+
+                        if (patient.GenListe.ContainsKey(currentGen))
+                        {
+                            patient.GenListe[currentGen]++;
+                        }
+                        else
+                        {
+                            patient.GenListe.Add(currentGen, 1);
+                        }
+                    }
+                }
+
+                patientList.Add(patient);
+            }
+
+            foreach (KeyValuePair<string, int> kvp in genList)
+            { 
+                Console.WriteLine(string.Format("{0} : {1}", kvp.Key, kvp.Value));
+
+            }
+
+            Console.WriteLine("Patienten: " + patientList.Count);
+
+           
+            DataTable dt = dgrid_DataGrid2.DataContext as DataTable;
+
+            csvLines.Add("ArrayID;Entitaet;");
+
+            foreach (KeyValuePair<string, int> kvp in genList)
+            {
+                csvLines[0] += kvp.Key + ";";
+            }
+
+            foreach (Patient p in patientList)
+            {
+
+                DataRow dr = dt.NewRow();
+                csvLines.Add( p.ArrayID + ";" + p.Entitaet + ";");
+
+                foreach (KeyValuePair<string, int> kvp in genList) 
+                {
+                    if (p.GenListe.ContainsKey(kvp.Key))
+                    {
+                        csvLines[csvLines.Count-1] += p.GenListe[kvp.Key];
+                    } 
+                    
+                    csvLines[csvLines.Count - 1] += ";";
+                }
+            }
+
+            WriteCSV(csvLines.ToArray());
+        }  
+        
+        private void WriteCSV(string[] lines)
+        {
+            System.IO.File.WriteAllLines("data.csv", lines);
         }
     }
 }
